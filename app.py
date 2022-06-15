f61ec22f687f5e1cd770c9c48b512e1b74e7f10a
Clair-Hawkes <hawkesj7@gmail.com>
2022-06-15 12:01:20 -0700

    Base Class and route route

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, render_template
+from models import db, connect_db,User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -9,4 +9,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
-db.create_all()
+# db.create_all()
+
+@app.get('/')
+def list_users():
+    """List users and show homepage"""
+
+    users = User.query.all()
+    return render_template("base.html",users=users)
+
+
--- models.py
+++ models.py
@@ -1,1 +1,24 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import false
+db = SQLAlchemy()
+
+class User (db.Model):
+    """Docstring????"""
+    __tablename__ = 'users'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True)
+    first_name = db.Column(
+        db.String(50),
+        nullable =false)
+    last_name = db.Column(
+        db.string(50),
+        nullable = false)
+    image_url = db.Column(
+        db.string(500),
+        nullable = false)
+
