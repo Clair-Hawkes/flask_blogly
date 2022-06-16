adb85dfb20ee0d66530a8a4d84e71bd7a769d53e
Clair-Hawkes <hawkesj7@gmail.com>
2022-06-16 11:27:06 -0700

    Add db object Post for tb posts

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 """Models for Blogly."""
 
+from xmlrpc.client import DateTime
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import false
+from sqlalchemy import ForeignKey, false
 db = SQLAlchemy()
 
 
@@ -28,3 +29,36 @@ class User (db.Model):
         nullable=false)
     image_url = db.Column(
         db.String(500))
+
+
+class Post (db.Model):
+    """Create post instances"""
+
+    __tablename__ = 'posts'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True)
+    title = db.Column(
+        db.String(50),
+        nullable=false)
+    content = db.Column(
+        db.Text,
+        nullable=false)
+    created_at=db.Column(
+        # Default server time?
+        db.DateTime,
+        nullable=false,
+        default=db.func.now)
+    # USer Foreign Key
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("users.id"))
+
+    user = db.relationship("User",backref='posts')
+
+
+
+
+
